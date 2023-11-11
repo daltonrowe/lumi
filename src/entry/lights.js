2edf2857c7af6037dd75d90cd5284dc23381ace1
Dalton <cdaltonrowe@gmail.com>
2023-11-11 12:52:14 -0500

    use class in node

--- src/entry/lights.js
+++ src/entry/lights.js
@@ -1,40 +1,14 @@
+const Effect = require("../effects/KnightRiderEffect");
 const ws281x = require("rpi-ws281x-native");
 
 const interval = 100;
-let counter = 0;
 
 const channel = ws281x(60, { stripType: "ws2812", brightness: 10 });
 const colorArray = channel.array;
 
-function getColor(num) {
-  switch (num) {
-    case 0:
-      return 0xff0000;
-
-    case 1:
-      return 0x00ff00;
-
-    case 2:
-      return 0x0000ff;
-
-    default:
-      return 0xffcc22;
-  }
-}
-
-function updateColor(color) {
-  for (let i = 0; i < channel.count; i++) {
-    colorArray[i] = color;
-  }
-}
+const effect = new Effect(colorArray);
 
 setInterval(() => {
-  if (counter >= 3) counter = 0;
-
-  const color = getColor(counter);
-  updateColor(color);
-
+  effect.compute();
   ws281x.render();
-
-  counter++;
 }, interval);
