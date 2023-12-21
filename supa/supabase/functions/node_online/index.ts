9dbdd923e23533925874105194b3aab6f3dd9ca3
Dalton <cdaltonrowe@gmail.com>
2023-12-21 12:41:56 -0500

    working node creation

--- supa/supabase/functions/node_online/index.ts
+++ supa/supabase/functions/node_online/index.ts
@@ -1,5 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { corsHeaders } from "../_shared/cors.ts";
+import { Database } from "../_shared/database.types.ts";
 
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
@@ -8,23 +9,45 @@ Deno.serve(async (req) => {
 
   const SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "";
   const SUPABASE_URL = Deno.env.get("SUPABASE_URL") ?? "";
-  const supabaseAdmin = createClient(SUPABASE_URL, SERVICE_ROLE_KEY);
+  const supabaseAdmin = createClient<Database>(SUPABASE_URL, SERVICE_ROLE_KEY);
 
   try {
-    const { node_code } = await req.json();
+    const { code } = await req.json();
 
-    const { data, error } = await supabaseAdmin
+    const { data: selectData, error: selectError } = await supabaseAdmin
       .from("nodes_v1")
-      .select("code")
-      .eq("code", node_code);
+      .select("code,id")
+      .eq("code", code);
 
-    if (error) {
-      console.log(error);
+    if (selectError) throw selectError;
 
-      throw error;
+    // node was found, updated is last alive time
+
+    if (selectData.length >= 1) {
+      const { data: updateData, error: updateError } = await supabaseAdmin
+        .from("nodes_v1")
+        .update({ updated_at: new Date().toISOString() })
+        .eq("id", selectData[0].id)
+        .select();
+
+      if (updateError) throw updateError;
+
+      return new Response(JSON.stringify(updateData), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200,
+      });
     }
 
-    return new Response(JSON.stringify(data), {
+    // node was not found, create it
+
+    const { data: insertData, error: insertError } = await supabaseAdmin
+      .from("nodes_v1")
+      .insert({ code })
+      .select();
+
+    if (insertError) throw insertError;
+
+    return new Response(JSON.stringify(insertData), {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
     });
