add24c2f9b2991e59b9f5a147bbf4b4d28ce6425
Dalton <cdaltonrowe@gmail.com>
2024-01-01 10:09:35 -0500

    combine docs

--- node/src/index.js
+++ node/src/index.js
@@ -2,7 +2,22 @@ require("dotenv").config();
 const { sendNodeOnline } = require("./util/sendNodeOnline");
 const { readCard } = require("./util/read");
 
+const Effect = require("./effects/RGBStrobeEffect");
+const ws281x = require("rpi-ws281x-native");
+
 sendNodeOnline();
+
+const interval = 100;
+
+const channel = ws281x(60, { stripType: "ws2812", brightness: 10 });
+const colorArray = channel.array;
+
+const effect = new Effect(colorArray);
+
+setInterval(() => {
+  effect.compute();
+  ws281x.render();
+}, interval);
 
 const tasks = [
   {
